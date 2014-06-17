3edd6c06b97e2fd2dd0c2cfa49ea8380d8147724
bign8 <big.nate.w@gmail.com>
2014-06-17 10:27:23 -0600

    Disable streetView

--- js.js
+++ js.js
@@ -93,7 +93,9 @@ var Map = function ( watch_obj, g_maps ) {
 	var initialize = function () {
 
 		// Fix map for USA
-		map = new g_maps.Map(document.getElementById('map-canvas'));
+		map = new g_maps.Map(document.getElementById('map-canvas'), {
+			streetViewControl: false
+		});
 		var default_bounds = new g_maps.LatLngBounds(
 			new g_maps.LatLng(49.38, 25.82),
 			new g_maps.LatLng(-124.38999999999999, -66.94)
